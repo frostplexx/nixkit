814902866c67734f1c656636b77c1661b79d443e
daniel <daniel.inama02@gmail.com>
2025-07-02 09:08:39 +0200

    fix: update cargo hash

--- modules/shared/opsops/default.nix
+++ modules/shared/opsops/default.nix
@@ -16,7 +16,7 @@ let
       sha256 = "sha256-sUEtf3q3oB2og1Ed8v7qg3K1nWZy2Rp30iwqxd4HBzk=";
     };
 
-    cargoHash = "sha256-7zTe2a9hKdaoAIUpifUYh1LPFBUTFMQBC3NYUnoK8/g=";
+    cargoHash = "sha256-S9HQwv9N33T1Q8c3dm7hQSyQzG0wIa7O0k/gaJa3S7w=";
 
     nativeBuildInputs = with pkgs; [ pkg-config sops ];
     buildInputs = with pkgs; [ openssl sops ];
@@ -25,13 +25,13 @@ let
       # Create directories for docs
       mkdir -p $out/share/man/man1
       mkdir -p $out/share/fish/vendor_completions.d
-      
+
       # Generate docs
       $out/bin/opsops generate-docs --dir $TMPDIR/docs
-      
+
       # Install man page
       cp $TMPDIR/docs/man/opsops.1 $out/share/man/man1/
-      
+
       # Install fish completions
       cp $TMPDIR/docs/completions/opsops.fish $out/share/fish/vendor_completions.d/
     '';
