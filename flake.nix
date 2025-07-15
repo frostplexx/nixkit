b899e95099509e3f5f97e20bbf17b399d4e05f4e
Josef Hofer <me@jhofer.de>
2025-07-15 13:31:36 +0200

    fix: has an override for a non-existent input 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Various modules and utilities for NixOS and nix-darwin";
 
     inputs = {
-
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
     outputs = { ... }: {
