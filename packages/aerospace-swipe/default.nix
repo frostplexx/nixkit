d8fe8d629dd95a31e3fa91641276a5923120789d
daniel <daniel.inama02@gmail.com>
2025-10-08 09:46:59 +0200

    fix: add darwin-sdk as build input

--- packages/aerospace-swipe/default.nix
+++ packages/aerospace-swipe/default.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  pkgs,
+  ...
 }:
 
 stdenv.mkDerivation {
@@ -15,6 +17,10 @@ stdenv.mkDerivation {
       hash = "sha256-ZLaE/CuUgpWXrmV0cKLI8L9R92REECxWcpOwofMDMx4=";
       rev =  "1845e0e99c4c4bb34453253189a437a698ddbdc8";
     };
+
+    nativeBuildInputs = [
+        pkgs.apple-sdk
+    ];
 
     buildPhase = ''
       make
