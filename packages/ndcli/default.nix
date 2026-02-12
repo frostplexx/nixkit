c75fe7be18f49a006265f55dd8c0c09c15b7586e
Josef Hofer <josef.hofer@1und1.de>
2026-02-12 16:29:33 +0100

    fix(pkgs/ndcli): bash and fish autocompletions

--- packages/ndcli/default.nix
+++ packages/ndcli/default.nix
@@ -31,6 +31,27 @@ python3Packages.buildPythonPackage rec {
     python3Packages.dnspython
   ];
 
+  postInstall = ''
+    # Install bash completions
+    mkdir -p $out/share/bash-completion/completions
+    cp bash_completion.d/ndcli $out/share/bash-completion/completions/ndcli
+
+    # Install fish completions
+    mkdir -p $out/share/fish/vendor_completions.d
+    cat > $out/share/fish/vendor_completions.d/ndcli.fish <<'EOF'
+# Fish completion for ndcli
+# ndcli uses bash programmable completion, so we wrap it for fish
+function __ndcli_complete
+    set -l cmd (commandline -cp)
+    set -l COMP_LINE "$cmd"
+    set -l COMP_POINT (string length "$COMP_LINE")
+    env COMP_LINE="$COMP_LINE" COMP_POINT=$COMP_POINT ndcli | string split ' '
+end
+
+complete -c ndcli -f -a '(__ndcli_complete)'
+EOF
+  '';
+
   meta = with lib; {
     description = "Command line interface for DIM (DNS and IP Management)";
     homepage = "https://github.com/ionos-cloud/dim";
