c31a7a55d8b5b4d6df2b0eb43240c4d6421ed5c3
Josef Hofer <me@jhofer.de>
2026-03-25 17:00:26 +0100

    fix(pkgs/sunshine-virt): put temp files into writable dir

--- packages/sunshine-virt-display/default.nix
+++ packages/sunshine-virt-display/default.nix
@@ -24,6 +24,19 @@ stdenv.mkDerivation {
     bash
   ];
 
+  postPatch = ''
+        substituteInPlace virt_display.sh \
+          --replace-fail 'sudo python3 "$PYTHON_SCRIPT"' \
+                         'sudo ${python3}/bin/python3 "$PYTHON_SCRIPT"'
+        substituteInPlace main.py \
+          --replace-fail 'SCRIPT_DIR = Path(__file__).parent.absolute()' \
+                         'SCRIPT_DIR = Path(__file__).parent.absolute()
+    DATA_DIR = Path("/var/lib/sunshine-virt-display")
+    DATA_DIR.mkdir(parents=True, exist_ok=True)' \
+          --replace-fail 'SCRIPT_DIR / "custom_edid.bin"' 'DATA_DIR / "custom_edid.bin"' \
+          --replace-fail 'SCRIPT_DIR / "virt_display.state"' 'DATA_DIR / "virt_display.state"'
+  '';
+
   installPhase = ''
     mkdir -p $out/share/sunshine-virt-display $out/bin
     cp -r * $out/share/sunshine-virt-display/
