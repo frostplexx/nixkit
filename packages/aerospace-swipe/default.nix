ed526309a6fd763bdacb30d69a08db54afead79a
Josef Hofer <me@jhofer.de>
2026-07-08 20:39:26 +0200

    update hash manually, fixing renovate some other time

--- packages/aerospace-swipe/default.nix
+++ packages/aerospace-swipe/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "acsandmann";
     repo = "aerospace-swipe";
-    hash = "sha256-0SvEebMAiD7+7bGow2EeSyKuwO3qoA0YNWm1UcNkYM4=";
+    hash = "sha256-ARJfYiWXBCvXA5JlFl/s4VIQ9xuqBoU3gPfC8B2mkWI=";
     rev = "976c3107f6ed9859149bdc130e3f8928f2ab6852";
   };
 
