0bdceccd020c737ef4ff03f8f470680999bf5ac2
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-07-13 08:38:33 +0200

    chore: update hash for ghostty

--- packages/ghostty-tip/default.nix
+++ packages/ghostty-tip/default.nix
@@ -11,7 +11,7 @@ stdenvNoCC.mkDerivation (_finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-macos-universal.zip";
-    hash = "sha256-Tde/5vdvVgUVFAVqnnNNLLWVtd0wPZPZc1LB7GYRiMw=";
+    hash = "sha256-hCkGBElMCVA05/MfSpmoIzI6tQNwAF4MMTDMLXr+b/Q=";
   };
 
   nativeBuildInputs = [unzip];
